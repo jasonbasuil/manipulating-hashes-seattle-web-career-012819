eca685d9e02bbfe9313f77b41e23433b1d14fcfd
Jason Basuil <basuilj@gmail.com>
2018-12-07 06:46:06 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,7 +13,15 @@ def first_challenge
     }
   }
 
-  #your code here
+  contacts.each do |person, data| 
+    data.each do |attribute, value| 
+      if attribute == :favorite_icecream_flavors 
+        if :favorite_icecream_flavors.include? ("strawberry")
+          :favorite_icecream_flavors.delete("strawberry")
+        end 
+      end 
+    end 
+  end 
 
 
   #remember to return your newly altered contacts hash!
